5d5125aa7e8b3ffea081e86e2aa1606682085e28
Muhammad Aufa <aufalfirdausi@gmail.com>
2025-07-13 14:21:26 +0700

    remaining: column email

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-
+const allDiv = document.querySelectorAll('.identity')
 const button = document.getElementById('btn-submit')
+const errorDiv = []
+const messageError = [
+    'First Name cannot be empty',
+    'Last Name cannot be empty',
+    'Looks like is not an email',
+    'Password cannot be empty'
+]
+const validEmail = 'muhammadaufa@gmail.com'
 
-button.addEventListener('click', function() {
-    const identity = this.querySelectorAll('indentity')
+allDiv.forEach((div, index) => {
+    const newDiv = document.createElement('div')
+    newDiv.className = 'error-message'
+    newDiv.innerHTML = `<span>${messageError[index]}</span>`
+    newDiv.style.display = 'flex'
+    newDiv.style.justifyContent = 'right'
+    newDiv.style.color = 'red'
+    newDiv.style.fontSize = '11px'
+    newDiv.style.fontStyle = 'italic'
+    newDiv.style.visibility = 'hidden'
+    errorDiv.push(newDiv)
+    div.after(newDiv)
+})
+
+
+button.addEventListener('click', () => {
+
+    button.style.backgroundColor = 'hsl(159, 53%, 66%)' 
+
+    setTimeout(() => {
+        button.style.backgroundColor = ""
+    }, 150)
+
+
+allDiv.forEach((div, index) => {
+
+    const input = div.querySelector('input')
+    const errorIcon = div.querySelector('img')
+
+    if (input && input.value.trim() === '') {
+        errorDiv[index].style.visibility = 'visible'
+        errorIcon.style.visibility = 'visible'
+    } else {
+        errorDiv[index].style.visibility = 'hidden'
+    }
+})
 
 })
+
